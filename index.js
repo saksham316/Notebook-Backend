bf0b8f185fc2bcae607a54e79f96949a8d20c143
saksham <phantomdx316@gmail.com>
2023-03-11 22:56:33 +0530

    added origin in cors

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 connectToMongo();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:process.env.BASE_URL
+}));
 app.use(express.json());
 
 app.use('/api/auth',require("./Routes/routes.js"));
