bd2525779063899ebf7e11f22aec0a2c11460b2c
saksham <phantomdx316@gmail.com>
2023-03-11 21:50:05 +0530

    added port in .env file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/api/auth',require("./Routes/routes.js"));
 app.use('/api/notes',require("./Routes/notes_route.js"));
 
 
-app.listen(PORT,()=>{
+app.listen(process.env.PORT,()=>{
     console.log(`App is listening at ${process.env.BASE_URL}`);
 });
 
