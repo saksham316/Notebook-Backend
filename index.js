f724960f3cd3914f72f59f2fdd78d0e06f07267c
saksham <phantomdx316@gmail.com>
2023-03-12 09:20:25 +0530

    removed testing url

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 connectToMongo();
 
 const app = express();
-app.use(cors({
-    origin:"http://localhost:3000"
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth',require("./Routes/routes.js"));
